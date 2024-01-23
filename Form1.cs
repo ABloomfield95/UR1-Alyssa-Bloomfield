34ef133fff1012fa0b0b6dbb10bc9207bb5f91c7
Alyssa Bloomfield <abloomfie@ltu.edu>
2024-01-23 11:20:25 -0500

    Added capture code and comments, but still had some issues

--- Form1.cs
+++ Form1.cs
@@ -49,19 +49,27 @@ namespace UR1_Alyssa_Bloomfield
 
         public partial class Form1 : Form
         {
+            //Main capture object
             VideoCapture mCapture;
 
+            //Video thread that allows multi-threading
             Thread mCaptureThread;
 
+            //Allows for thread termination
             CancellationTokenSource mCallellationToken = new();
 
+            //Indicator for the capturing state
             bool mIsCapturing = false;
         }
+
+        private Button StartStopBtn;
+        private PictureBox VideoPictureBox;
 
         private void Form1_Load(object sender, EventArgs e)
         {
             try
             {
+                //Initailize with ifany plugged camera
                 mCapture = new VideoCapture(0);
                 if (mCapture.Height == 0)
                     throw new Exception("No Cameras Found");
@@ -73,38 +81,46 @@ namespace UR1_Alyssa_Bloomfield
         }
         private void StartStopBtn_Click(object sender, EventArgs e)
         {
+            //Stop capturing if live 
             if (mIsCapturing)
             {
-                mCancellationToken.Cancel();
-                mIsCapturing = false;
-                StartStopBtn.Text = "Start";
+                mCancellationToken.Cancel(); //requesting for a new stop
+                mIsCapturing = false; //indicate a new state
+                StartStopBtn.Text = "Start"; //inform accordingly
             }
+            //Start capturing if dead
             else
             {
-                mCancellationToken = new();
-                mCaptureThread = new(() => DisplayWebCam(mCancellationToken.Token));
-                mCapture.Thread.start();
-                mIsCapturing = true;
-                StartStopBtn.Text = "Stop";
+                mCancellationToken = new(); // reinitialize
+                
+                //Create a new thread(inilitize)
+                mCaptureThread = new(() => DisplayWebCam(mCancellationToken.Token)); 
+                mCapture.Thread.start();//start the feed
+                mIsCapturing = true; //indicate the state
+                StartStopBtn.Text = "Stop"; //infrom accordingly
             }
         }
         private void DisplayWebcam(CancellationToken token)
         {
-            while (!token.IsCancellationRequested)
+            while (!token.IsCancellationRequested) //While no requested cancellation
             {
-                Mat frame = mCapture.QueryFrame();
+                Mat frame = mCapture.QueryFrame(); //grab a new frame
 
-                int newHeight = frame(frame.Size.Height * PictureBox.Size.Width) / frame.Size.Width;
+                //resize picture box
+                int newHeight = frame(frame.Size.Height * PictureBox.Size.Width) / frame.Size.Width; 
                 Size newSize = new Size(PictureBox.Size.Width, newHeight);
                 CVInvoke.Resize(frame, frame, newSize);
 
+                //Create a 60 fps frame rate
                 Task.Delay(16);
 
+                //Display the current frame
                 PictureBox.Image = frame.ToBitmap();
             }
         }
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
+            //Dispose all processing threats to avoid orphanded processes
             if (mIsCapturing)
             {
                 mCancellationToken.Cancel();
@@ -114,14 +130,6 @@ namespace UR1_Alyssa_Bloomfield
                 mCapture.Dispose();
                 mCancellationToken.Dispose();
             }
-        }
-
-        private Button StartStopBtn;
-        private PictureBox VideoPictureBox;
-
-        private void VideoPictureBox_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void StartStopBtn_Click_1(object sender, EventArgs e)
