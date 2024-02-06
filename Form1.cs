16aca51c91342ee557d7e724769834c3be96d6be
Alyssa Bloomfield <abloomfie@ltu.edu>
2024-02-06 15:24:47 -0500

    Made edits to add the grayscale picture and the slider bars

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using Emgu.CV;
+using Emgu.CV.CvEnum;
 
 namespace UR1_Alyssa_Bloomfield
 {
@@ -19,13 +20,32 @@ namespace UR1_Alyssa_Bloomfield
 
         private Button StartStopBtn;
         private PictureBox VideoPictureBox;
+
+        CancellationTokenSource _CancellationToken = new();
+
+        //For the black and white picture box
         private PictureBox GrayPictureBox;
 
+        private int minGrayValue = 10;
+        private int maxGrayValue = 255;
+
+        private TrackBar MinTrackBar;
+        private TrackBar MaxTrackBar;
 
         public Form1()
         {
             InitializeComponent();
         }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
 
         //Backend Setup given by MSVS
         private void InitializeComponent()
@@ -33,13 +53,17 @@ namespace UR1_Alyssa_Bloomfield
             StartStopBtn = new Button();
             VideoPictureBox = new PictureBox();
             GrayPictureBox = new PictureBox();
+            MinTrackBar = new TrackBar();
+            MaxTrackBar = new TrackBar();
             ((System.ComponentModel.ISupportInitialize)VideoPictureBox).BeginInit();
             ((System.ComponentModel.ISupportInitialize)GrayPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)MinTrackBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)MaxTrackBar).BeginInit();
             SuspendLayout();
             // 
             // StartStopBtn
             // 
-            StartStopBtn.Location = new Point(313, 683);
+            StartStopBtn.Location = new Point(332, 715);
             StartStopBtn.Name = "StartStopBtn";
             StartStopBtn.Size = new Size(207, 108);
             StartStopBtn.TabIndex = 0;
@@ -63,9 +87,33 @@ namespace UR1_Alyssa_Bloomfield
             GrayPictureBox.TabIndex = 2;
             GrayPictureBox.TabStop = false;
             // 
+            // MinTrackBar
+            // 
+            MinTrackBar.Location = new Point(1033, 655);
+            MinTrackBar.Maximum = 255;
+            MinTrackBar.Minimum = 100;
+            MinTrackBar.Name = "MinTrackBar";
+            MinTrackBar.Size = new Size(545, 114);
+            MinTrackBar.TabIndex = 3;
+            MinTrackBar.Value = 100;
+            MinTrackBar.Scroll += trackBar1_Scroll;
+            // 
+            // MaxTrackBar
+            // 
+            MaxTrackBar.Location = new Point(1033, 794);
+            MaxTrackBar.Maximum = 255;
+            MaxTrackBar.Minimum = 100;
+            MaxTrackBar.Name = "MaxTrackBar";
+            MaxTrackBar.Size = new Size(545, 114);
+            MaxTrackBar.TabIndex = 4;
+            MaxTrackBar.Value = 100;
+            MaxTrackBar.Scroll += trackBar2_Scroll;
+            // 
             // Form1
             // 
-            ClientSize = new Size(1757, 886);
+            ClientSize = new Size(1882, 984);
+            Controls.Add(MaxTrackBar);
+            Controls.Add(MinTrackBar);
             Controls.Add(GrayPictureBox);
             Controls.Add(VideoPictureBox);
             Controls.Add(StartStopBtn);
@@ -73,15 +121,19 @@ namespace UR1_Alyssa_Bloomfield
             Load += Form1_Load_1;
             ((System.ComponentModel.ISupportInitialize)VideoPictureBox).EndInit();
             ((System.ComponentModel.ISupportInitialize)GrayPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)MinTrackBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)MaxTrackBar).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         private void Form1_Load_1(object sender, EventArgs e)
         {
             try
             {
-                //Initailize with ifany plugged camera
+                //Initailize with the plugged camera
                 mCapture = new VideoCapture(0);
+
                 if (mCapture.Height == 0)
                     throw new Exception("No Cameras Found");
             }
@@ -125,38 +177,38 @@ namespace UR1_Alyssa_Bloomfield
                 Size newSize = new Size(VideoPictureBox.Size.Width, newHeight);
                 CvInvoke.Resize(frame, frame, newSize);
 
+                //resize gray picture box
+                int newGHeight = (frame.Size.Height * GrayPictureBox.Size.Width) / frame.Size.Width;
+                Size newGSize = new Size(GrayPictureBox.Size.Width, newHeight);
+
                 //Create a 60 fps frame rate
                 Task.Delay(16);
 
                 //Display the current frame
                 VideoPictureBox.Image = frame.ToBitmap();
-            }
-        }
 
-        private void DisplayGrayWebcam(CancellationToken token)
-        {
-            while (!token.IsCancellationRequested) //While no requested cancellation
-            {
-                Mat frame = mCapture.QueryFrame(); //grab a new frame
-
-                //resize picture box
-                int newHeight = (frame.Size.Height * VideoPictureBox.Size.Width) / frame.Size.Width;
-                Size newSize = new Size(VideoPictureBox.Size.Width, newHeight);
-                CvInvoke.Resize(frame, frame, newSize);
-
-                CvInvoke.CvtColor(frame, frame, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
-                CvInvoke.Threshold(frame, frame, 150, 255, Emgu.CV.CvEnum.ThreasholdType.Binary);
-
-                //Create a 60 fps frame rate
-                Task.Delay(16);
-
-                //Display the current frame
+                CvInvoke.CvtColor(frame, frame, ColorConversion.Bgr2Gray);
+                CvInvoke.Threshold(frame, frame, minGrayValue,maxGrayValue,Emgu.CV.CvEnum.ThresholdType.Binary);
                 GrayPictureBox.Image = frame.ToBitmap();
-            }
-        }
+
+                Invoke(new Action(() =>
+                    {
+                        if (MinTrackBar.Value > MaxTrackBar.Value)
+                        {
+                            MinTrackBar.Value = MaxTrackBar.Value;
+                        }
+
+                minGrayValue = MinTrackBar.Value;
+                maxGrayValue = MaxTrackBar.Value;
+                    }
+                ));
+            } //while loop closer
+        } //private void closer
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
+            mCaptureThread.Abort();
+
             //Dispose all processing threats to avoid orphanded processes
             if (mIsCapturing)
             {
